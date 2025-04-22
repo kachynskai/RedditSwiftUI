d50982201e44971470e2fdd78e32914fdff8a3c9
kachynskai <105295714+kachynskai@users.noreply.github.com>
2025-04-22 17:11:45 +0300

    Initial Commit

---
+++ RedditSwiftUI/RedditSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedditSwiftUIApp.swift
+//  RedditSwiftUI
+//
+//  Created by Iryna on 22.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct RedditSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
