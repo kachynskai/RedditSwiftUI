0206be9cae3600ad8be127efc77c3cf872401fed
kachynskai <kachynskaiv2@gmail.com>
2025-04-30 06:02:03 +0300

    done lab 6

---
+++ RedditSwiftUI/AddPostView.swift
@@ -0,0 +1,18 @@
+//
+//  AddPostView.swift
+//  RedditSwiftUI
+//
+//  Created by Iryna on 22.04.2025.
+//
+
+import SwiftUI
+
+struct AddPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddPostView()
+}
---
+++ RedditSwiftUI/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  RedditSwiftUI
+//
+//  Created by Iryna on 22.04.2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
